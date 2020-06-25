0d01b7812a9a9da32bfe7dd538f8e37648bf4ca0
Orosz Gyorgy <yuryka2@gmail.com>
2020-06-25 10:03:10 +0300

    edit working - checked working - details working

--- src/main/java/ro/fasttrackit/curs25/movies/web/EventUIController.java
+++ src/main/java/ro/fasttrackit/curs25/movies/web/EventUIController.java
@@ -3,7 +3,12 @@ package ro.fasttrackit.curs25.movies.web;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import ro.fasttrackit.curs25.movies.domain.Event;
+import ro.fasttrackit.curs25.movies.domain.Movie;
 import ro.fasttrackit.curs25.movies.service.EventService;
+
+import java.util.Optional;
 
 @Controller
 public class EventUIController {
@@ -21,7 +26,19 @@ public class EventUIController {
 
     @GetMapping("organizer")
     public String organizerPage(Model pageModel) {
-       pageModel.addAttribute("events", localEventService.getAll());
+        pageModel.addAttribute("events", localEventService.getAll());
         return "event";
+    }
+
+    @GetMapping("events/{id}")
+    public String eventPageWithDetails(@PathVariable Integer id, Model pageModel) {
+        Optional<Event> event = localEventService.getEvent(id);
+        if (event.isPresent()) {
+            pageModel.addAttribute("showDetails", true);
+            pageModel.addAttribute("selectedEvent", localEventService.getEventOrThrow(id));
+            return organizerPage(pageModel);
+        } else {
+            return "redirect:/organizer";
+        }
     }
 }
