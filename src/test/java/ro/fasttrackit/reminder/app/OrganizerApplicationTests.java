646f4d0d2183ae4d3b92b7ee755170d37e37f932
Orosz Gyorgy <yuryka2@gmail.com>
2020-07-10 22:29:31 +0300

    +test

---
+++ src/test/java/ro/fasttrackit/reminder/app/OrganizerApplicationTests.java
@@ -0,0 +1,12 @@
+package ro.fasttrackit.reminder.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrganizerApplicationTest {
+
+    @Test
+    void contextLoads() {
+    }
+}
